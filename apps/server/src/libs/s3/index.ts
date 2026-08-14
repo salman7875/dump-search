a8af2eede1ec0182eebca3b058bae054c45dfea4
Salman Ansari <asalman7875@gmail.com>
2026-08-14 20:06:02 +0530

    feat: api for generating upload file url

--- apps/server/src/libs/s3/index.ts
+++ apps/server/src/libs/s3/index.ts
@@ -1,5 +1,7 @@
 import { S3Client } from '@aws-sdk/client-s3';
 
-const s3Client = new S3Client({ region: process.env.AWS_REGION || "us-east-1" });
+const s3Client = new S3Client({
+  region: process.env.AWS_REGION || 'us-east-1',
+});
 
 export default s3Client;
--- apps/server/src/modules/upload/upload.controllers.ts
+++ apps/server/src/modules/upload/upload.controllers.ts
@@ -1,5 +1,25 @@
 import type { Request, Response } from 'express';
 import { fileProcessQueue, queueType } from '@repo/queue';
+import { PutObjectCommand } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import s3Client from '../../libs/s3/index.js';
+
+const getUploadUrl = async (req: Request, res: Response) => {
+  try {
+    const { fileName, fileType } = req.body;
+    const s3Key = `uploads/${Date.now()}-${fileName}`;
+    const command = new PutObjectCommand({
+      Bucket: 'my-bucket',
+      Key: s3Key,
+      ContentType: fileType,
+    });
+
+    const uploadUrl = getSignedUrl(s3Client, command, { expiresIn: 300 });
+    res.status(201).json({ success: true, data: { uploadUrl } });
+  } catch (err) {
+    res.status(500).json({ success: false, message: err });
+  }
+};
 
 const uploadDoc = async (req: Request, res: Response) => {
   try {
